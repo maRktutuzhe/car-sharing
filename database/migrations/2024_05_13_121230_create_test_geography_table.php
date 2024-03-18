4fc3c8fd3609969af98d13a5f99519b1b17b6db1
blankbuffoon <aterepin@yandex.ru>
2024-03-18 15:49:56 +0500

    Install postgis image in docker-compose.yml

---
+++ database/migrations/2024_05_13_121230_create_test_geography_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('test_geography', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->point('coords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prices');
+    }
+};
